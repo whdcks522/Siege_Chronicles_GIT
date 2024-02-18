fa9da381eadd1289f172cbe621a5102485d031f5
whdcks522 <whdcks522@naver.com>
2024-02-18 19:48:22 +0900

    맵 준비 완료

--- Assets/Resources/Scripts/CameraManager.cs
+++ Assets/Resources/Scripts/CameraManager.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public GameObject cameraObj;
-    public int i;
+    public Transform cameraObj;
+    public Transform cameraParent;
+    public Transform cameraTarget1;
+    public Transform cameraTarget2;
+
+    public int rot;
+    int mul = 25;//카메라 회전 배율
+    Vector3 cameraVec;
+
+    private void Update()
+    {
+        //카메라 위치 관리
+        cameraVec = mul * new Vector3(Mathf.Sin(Mathf.PI * rot / 360), 0, Mathf.Cos(Mathf.PI * rot / 360));
+        cameraObj.position = cameraParent.position + cameraVec;
+
+        //시야 관리
+        cameraObj.LookAt((cameraTarget1.transform.position + cameraTarget2.transform.position) / 2f);
+    }
+
 
     #region 난사 발사
-    IEnumerator genocideShot(string bulletName, GameObject host, GameObject target, int index)
-    {
-        yield return null;
 
-        Vector2 tmpVec = new Vector2(Mathf.Sin(Mathf.PI * i / index * 2.0f), Mathf.Cos(Mathf.PI * i / index * 2.0f));
-    }
     #endregion
 }
