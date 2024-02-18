92cc83d36baec826fc509c9a3258a3b8418ba16e
whdcks522 <whdcks522@naver.com>
2024-02-18 18:43:14 +0900

    Update CameraManager.cs

--- Assets/Resources/Scripts/CameraManager.cs
+++ Assets/Resources/Scripts/CameraManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cameraObj;
+    public int i;
 
-    // Update is called once per frame
-    void Update()
+    #region 난사 발사
+    IEnumerator genocideShot(string bulletName, GameObject host, GameObject target, int index)
     {
-        
+        yield return null;
+
+        Vector2 tmpVec = new Vector2(Mathf.Sin(Mathf.PI * i / index * 2.0f), Mathf.Cos(Mathf.PI * i / index * 2.0f));
     }
+    #endregion
 }
